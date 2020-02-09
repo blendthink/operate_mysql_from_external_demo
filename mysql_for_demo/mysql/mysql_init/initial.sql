a23dff44991dfd8baf6265d5fa235feff689cb78
Tatsuya Okayama <t_okayama@yumemi.co.jp>
2020-02-10 03:02:39 +0900

    デモ用の MySQL 関連のファイルを追加

---
+++ mysql_for_demo/mysql/mysql_init/initial.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `models` (
+    `id` int NOT NULL,
+    `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `models` VALUES
+    (1, 'demo1'),
+    (2, 'demo2'),
+    (3, 'demo3'),
+    (4, 'demo4'),
+    (5, 'demo5')
+;
